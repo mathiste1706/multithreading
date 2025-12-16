2b64485a7896cdc06416738a1c544b3e28861cbf
mathiste1706 <92776941+mathiste1706@users.noreply.github.com>
2025-12-16 17:13:19 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 import time
-
+import json
 import numpy as np
 
 
@@ -19,3 +19,31 @@ class Task:
         start = time.perf_counter()
         self.x = np.linalg.solve(self.a, self.b)
         self.time = time.perf_counter() - start
+
+    def to_json(self) -> str:
+        return json.dumps(
+            {
+                "identifier": self.identifier,
+                "size": self.size,
+                "time": self.time,
+                "a": self.a.tolist(),
+                "b": self.b.tolist(),
+                "x": self.x.tolist(),
+            }
+        )
+
+    @staticmethod
+    def from_json(text: str) -> "Task":
+        data = json.loads(text)
+
+        task = Task(
+            identifier=data["identifier"],
+            size=data["size"],
+        )
+
+        task.time = data["time"]
+        task.a = np.array(data["a"])
+        task.b = np.array(data["b"])
+        task.x = np.array(data["x"])
+
+        return task
--- test_task.py
+++ test_task.py
@@ -1,22 +1,17 @@
 import unittest
-import numpy as np
 from numpy.testing import assert_allclose
 from task import Task
 
 
-
 class TestTask(unittest.TestCase):
-
     def test_Ax_equals_B(self):
-        
         task = Task(1, 10)
 
         Task.work(task)
 
         # Vérification numérique avec tolérance
-        assert_allclose(task.a @task.x, task.b, rtol=1e-7, atol=1e-9)
+        assert_allclose(task.a @ task.x, task.b, rtol=1e-7, atol=1e-9)
 
 
 if __name__ == "__main__":
     unittest.main()
-
